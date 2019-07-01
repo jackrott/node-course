ed657d37623761ec8286f2fe4fdc883f64f37857
jackrott <hernan.andres.s@gmail.com>
2019-07-01 13:35:05 -0400

    Analizando parametros con modulo yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,8 +9,21 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function(){
-        console.log('Adding a new note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title:', argv.title)
+        console.log('Body:', argv.body)
     }
 })
 
@@ -40,7 +53,8 @@ yargs.command({
         console.log('Reading the note!')
     }
 })
-console.log(yargs.argv)
+
+yargs.parse()
 
 /*if(commnad === 'add') {
     console.log('adding note')
