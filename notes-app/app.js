feb9c81943542d9ac2fcc30a4ff4c229304240cc
jackrott <hernan.andres.s@gmail.com>
2019-07-01 13:12:40 -0400

    Implementación de modulo yargs.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,49 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const commnad = process.argv[2];
 
-if(commnad === 'add') {
+yargs.version('1.1.0')
+
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note!')
+    }
+})
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note!')
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function(){
+        console.log('Listing note!')
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading the note!')
+    }
+})
+console.log(yargs.argv)
+
+/*if(commnad === 'add') {
     console.log('adding note')
 } else if(commnad === 'remove') {
     console.log('removing note')
-}
+}*/
