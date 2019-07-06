57cd0dcd8df3a29501f7d24703e12acd89fffbca
jackrott <hernan.andres.s@gmail.com>
2019-07-06 17:26:18 -0400

    Consumir servicio de meteorologia y pequeños ajustes de salida de la información.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/9532abce5cac837dab75119a5c1f8ca2/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/9532abce5cac837dab75119a5c1f8ca2/37.8267,-122.4233?units=si&lang=es'
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
+request({url: url, json: true}, (error, response) => {
+    const currently = response.body.currently;
+    console.log(response.body.daily.data[0].summary + ' Temperatura actual ' +   currently.temperature + ' grados celsius. Con una probabilidad de ' + currently.precipProbability + ' de precipitaciones') 
 })
