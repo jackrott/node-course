d674d2f08e52f0a7d92f9e2a0b5171251917656e
jackrott <hernan.andres.s@gmail.com>
2019-07-07 21:08:04 -0400

    Obtener latitud y longitud desde servicio mapbox

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,3 +6,13 @@ request({url: url, json: true}, (error, response) => {
     const currently = response.body.currently;
     console.log(response.body.daily.data[0].summary + ' Temperatura actual ' +   currently.temperature + ' grados celsius. Con una probabilidad de ' + currently.precipProbability + ' de precipitaciones') 
 })
+
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamFja3JvdHQiLCJhIjoiY2p4dG5jbGNmMHdoaDNndDVjOXV3Ym1neSJ9.C6EevASIEw-Aa6Pk9GA5Og&limit=1'
+
+request({url: geocodeURL, json: true}, (error, response) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    
+    console.log(latitude, longitude) 
+})
