2e2b373ded3ae199732b8f696fa4a8b2c627c528
jackrott <hernan.andres.s@gmail.com>
2019-07-08 20:25:24 -0400

    callback abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 /*const url = 'https://api.darksky.net/forecast/9532abce5cac837dab75119a5c1f8ca2/37.8267,-122.4233?units=si&lang=es'
 
@@ -19,7 +20,7 @@ request({url: url, json: true}, (error, response) => {
  * geocode
  * Address => lat, long -> weather
  */
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamFja3JvdHQiLCJhIjoiY2p4dG5jbGNmMHdoaDNndDVjOXV3Ym1neSJ9.C6EevASIEw-Aa6Pk9GA5Og&limit=1'
+/*const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamFja3JvdHQiLCJhIjoiY2p4dG5jbGNmMHdoaDNndDVjOXV3Ym1neSJ9.C6EevASIEw-Aa6Pk9GA5Og&limit=1'
 
 request({url: geocodeURL, json: true}, (error, response) => {
     if(error) {
@@ -31,4 +32,10 @@ request({url: geocodeURL, json: true}, (error, response) => {
         const longitude = response.body.features[0].center[0]
         console.log(latitude, longitude) 
     }
+})*/
+
+
+geocode('Talagante', (error, data) => {
+    console.log('error', error)
+    console.log('data', data)
 })
