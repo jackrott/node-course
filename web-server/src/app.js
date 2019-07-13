cfec98aadde887091259dfe4c839cbb2bbd4394f
jackrott <hernan.andres.s@gmail.com>
2019-07-13 17:08:41 -0400

    Implementación plantillas hbs - Contenido dinamico

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,12 +3,35 @@ const express = require('express')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 /**
  * app.com
  * app.com/help
  * app.com/about
  */
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Hernán Soto'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Sobre mí',
+        name: 'Hernán Soto'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'AYUDA proporcionada por Maximiliano Andres'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
