2d89b9b1a760347350bbc7248db6743bff57e1d4
jackrott <hernan.andres.s@gmail.com>
2019-07-17 20:58:19 -0400

    Creación y control de paginas no encontradas (404 not found)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -50,6 +50,22 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: 'Not found',
+        name: 'Hernán Soto',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'Not found',
+        name: 'Hernán Soto',
+        errorMessage: 'Page not found'
+    })
+})
+
 app.listen(3000, () => {
     console.log('server is up on port 3000')
 })
