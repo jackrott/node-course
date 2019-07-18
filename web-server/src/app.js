be3eab2db624d23b740382603575925ce71ee244
Hernan Soto <hsoto@odepa.gob.cl>
2019-07-18 08:44:02 -0400

    Construcción de endpoint que consume servicio de pronostico de tiempo para una dirección especifica

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -47,16 +49,35 @@ app.get('/weather', (req, res) => {
     
     if(!req.query.address) {
         return res.send({
-            error: 'You must provide a address term'
+            error: 'You must provide an address'
         })
     }
     
+    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+        if(error) {
+            return res.send({ error })
+        }
     
-    res.send({
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) {
+                return res.send({ error })
+            }
+    
+            res.send({
+                forecast: forecastData,
+                location: location,
+                address: req.query.address
+            })
+        })
+    })
+    
+
+
+    /*res.send({
         forecast: 'lluvia',
         location: 'talagante',
         address: req.query.address
-    })
+    })*/
 })
 
 app.get('/products', (req, res) => {
