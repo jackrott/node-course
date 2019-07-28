db3d45d099f063d5a128032dad8526cb42562957
jackrott <hernan.andres.s@gmail.com>
2019-07-27 21:35:51 -0400

    Se añade temperatura mínima y máxima

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -11,7 +11,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Pronostico no encontrado para latitud:' + latitude + ', longitud: ' + longitude, undefined)
         }else {
             const currently = body.currently;
-            callback(undefined, body.daily.data[0].summary + ' Temperatura actual ' +   currently.temperature + ' grados celsius. Con una probabilidad de ' + currently.precipProbability + ' de precipitaciones') 
+            console.info(body.daily.data[0]);
+            callback(undefined, body.daily.data[0].summary + ' Temperatura actual ' +   currently.temperature + ' grados celsius. Temperatura mínima: ' + body.daily.data[0].temperatureMin + ' y temperatura máxima: ' + body.daily.data[0].temperatureMax + '. Con una probabilidad de ' + currently.precipProbability + ' de precipitaciones') 
         }
     })
 }
