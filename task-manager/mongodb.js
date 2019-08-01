6898b0955cadae09d8465be3a9303c739f6bd529
jackrott <hernan.andres.s@gmail.com>
2019-07-31 21:22:10 -0400

    Realizar búsqueda de documentos en mongodb

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,9 +1,13 @@
 //crud
-const mongodb = require('mongodb')
+/*const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID*/
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
     if(error) {
@@ -11,8 +15,20 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => 
     }
 
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: 'Hernan',
-        age: 33
+    
+    /*db.collection('users').findOne({name: 'Hernan'}, (error, user) => {
+        if(error) {
+            return console.log('problemas al obtener la información del usuario')
+        }
+
+        console.log(user)
+    })*/
+    
+    db.collection('task').findOne({ _id: new ObjectID("5d42319d824c5e72c78ecb89") }, (error, task) => {
+        console.log(task)
+    })
+
+    db.collection('task').find({completed: false}).toArray((error, task) => {
+        console.log(task)
     })
 })
