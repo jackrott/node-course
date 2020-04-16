4f76ea03ba5a8f1496ba754559fb6f8bdf488016
jackrott <hernan.andres.s@gmail.com>
2020-04-16 12:40:20 -0400

    Usando validadores en schemas

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
@@ -7,10 +8,30 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
@@ -23,13 +44,14 @@ const Task = mongoose.model('Task', {
     }
 })
 
-const task = new Task({
-    description: 'Learn the mongoose library',
-    completed: false
+const me = new User({
+    name: 'Hernán Andres',
+    email: 'HERnan.andres.s@gmail.com',
+    age: 34
 })
 
-task.save().then((result) => {
-    console.log(result)
+me.save().then((result) => {
+    console.log("Usuario registrado exitosamente")
 }).catch((error) => {
     console.log(error)
 })
