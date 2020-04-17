2ec19b76b71c3c3b8aaebcaa3033c39e240c2053
jackrott <hernan.andres.s@gmail.com>
2020-04-17 11:46:27 -0400

    Creación de modelo task y endpoint crear task

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./db/mongoose')
 
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,11 +13,24 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then((result) => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e)
     })
 })
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then((result) => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+
+
 
 app.listen(port, () => {
     console.log('Server is UP in port ' + port)
