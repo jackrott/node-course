43424f6e45d4dcdcdfc6395d84eec477bd05864b
jackrott <hernan.andres.s@gmail.com>
2020-04-23 17:07:36 -0400

    Filtros, ordenamiento y paginación

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -16,17 +16,31 @@ router.post('/tasks', auth, async(req, res) => {
     }
 })
 
-
+// filter: /tasks?completed=true
+// pagination: /tasks?limit=2&skip=2
+// sort: /tasks?sortBy=description:desc
 router.get('/tasks', auth, async(req, res) => {
     const match = {}
+    const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
     }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
         await req.user.populate({
             path: 'tasks',
-            match: match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (error) {
